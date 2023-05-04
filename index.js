ec8516c591bdaa4323d78d768dc6cc4085ab8e78
fatema-nasrin <nasrinfatema35@gmail.com>
2023-05-04 13:20:56 +0600

    one file deleted

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require ('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+const chefData = require('./chef-data.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('dragon is running')
 });
+
+app.get('/chefs',(req, res) => {
+    res.send(chefData);
+})
 
 app.listen(port, () => {
     console.log('dragon api is running: ${port}')
